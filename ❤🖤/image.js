291d141889bf40d4e1bd17ceac0bed70b99d2fa5
nivesharn <arndas777@gmail.com>
2024-08-28 18:40:45 +0530

    d

--- ❤🖤/image.js
+++
@@ -1,9 +0,0 @@
-const downloadButton = document.getElementById('downloadButton');
-const imageUrl = "D:\❤🖤\one.jpg"; 
-
-downloadButton.addEventListener('click', () => {
-  const link = document.createElement('a');
-  link.href = imageUrl;
-  link.download = 'image.jpg'; // Optional: Specify download filename
-  link.click();
-});
